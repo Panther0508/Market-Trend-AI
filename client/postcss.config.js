ddd2c564ac87927b197ed57f635d85d3da4609b5
Ngbaronye Nmesirionye <pantherlord@gmail.com>
2026-03-17 11:30:08 +0100

    feat: Configure PostCSS with Tailwind CSS and Autoprefixer, and disable CSS code splitting in Vite.

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,11 +1,15 @@
-const path = require('path');
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   plugins: {
     tailwindcss: {
-      config: path.resolve(__dirname, '..', 'tailwind.config.ts'),
+      config: path.resolve(__dirname, "..", "tailwind.config.ts"),
     },
     autoprefixer: {},
   },
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    cssCodeSplit: false,
   },
   server: {
     fs: {
